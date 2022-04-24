b03bd9ffd616d5df4f83a7edde975569afbbe3ab
Eder Lima <devilsouls666@gmail.com>
2022-04-24 15:07:58 -0300

    feat: add models, admin page and update global settings

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from core.models import Company, Job
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "image",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("name", "address")
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "full_time",
+        "remote",
+        "company",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("title", "description")
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,37 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="company_images")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Companies"
+
+
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    application_url = models.URLField()
+
+    full_time = models.BooleanField(default=True)
+    remote = models.BooleanField(default=False)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
